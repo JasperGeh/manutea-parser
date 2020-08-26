1177d011554ec643d4301e1f19a150d7c3bee59c
JasperGeh <jasper.goetting@gmail.com>
2020-08-26 16:26:27 +0200

    Finished Basic App

--- app.R
+++ app.R
@@ -1,55 +1,98 @@
+# Libraries ####
 rm(list = ls())
 library(shiny)
 library(dplyr) # for quickly adding split genes as column to df
 library(container) # includes dict data structure
 library(data.table) # for fast transposing of dfs
+library(stringr) # for string replacement
 
 
+# UI ####
 ui <- fluidPage(
-  numericInput(inputId = "Gene", 
-              label = "Select Gene", 
-              value = 1, min = 1, max = 5),
-  tableOutput(outputId = "tabl")
+  h1(style="text-align: center;", "Manutea's GO Parser"),
+  p(style="text-align: center;", "Hey Manutea! Load the CSV, hit 'Analyse', and switch to the 'Analysis'-tab to select a gene and see the sorted ontologies.
+    Let me know if you need more or something doesn't work. —Jasper"),
+  fluidRow(
+    column(10, 
+           align='center', 
+           offset = 1, 
+           fileInput('datafile', 'Choose CSV file',
+              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+           actionButton("go", "Analyse"))),
+  tabsetPanel(
+    tabPanel("Input Table",
+             dataTableOutput(outputId = 'inTab')),
+    tabPanel("Analysis",
+             br(),
+             wellPanel(uiOutput("gene")),
+             dataTableOutput(outputId = 'resTab'))
+    )
 )
 
 
+# Server #####
 server <- function(input, output) {
-  df <- data.frame(Ontology = c("GO1", "GO2", "GO3", "GO4", "GO5", "GO6", "GO7"), 
-                   Genes = c("A, B", "A, D", "C", "A, D, F", "D, C, F", "C, F", "A, B, F"), 
-                   pValue = c(0.001, 0.06, 0.000002, 0.5, 0.00015, 0.0004, 0.01), stringsAsFactors=FALSE)
-  
-  df <- df %>%
-    mutate(Genelist = strsplit(Genes, ", ")) # create new column Genelist which splits Gene-column at commas
-  
-  genedict <- dict() # Create empty dict from container library
-  
-  for (i in 1:nrow(df)) { # Iterate through rows of the DF
-    l = df[[i,4]] # save list of split genes as "l"
-    for (e in l) { # Iterate through list of genes
-      if (!genedict$has(e)) { # check if dict already has gene in it and if not
-        genedict$set(e, list(c(df[[i,1]], df[[i,3]])), add=TRUE) # add GO with p-Value to dict as list
-      } else { # check if pValue of current row is smaller than previously saved and if yes
-        curr <- genedict$peek(e) # save current dict content for that gene
-        genedict$set(e, append(curr, list(c(df[[i,1]], df[[i,3]]))), add=TRUE) # append current GO with p-Value to previous content and save
+
+  filedata <- reactive({
+    infile <- input$datafile
+    if (is.null(infile)) {
+      # User has not uploaded a file yet
+      return(NULL)
+    }
+    df <- read.csv(infile$datapath, stringsAsFactors = FALSE)
+    df <- df %>%
+      mutate(genes = str_replace_all(genes, "\\|", ", ")) %>%
+      mutate(genes = strsplit(genes, ", ")) # create new column Genelist which splits Gene-column at commas
+    df
+  })
+  res <- eventReactive(input$go, {
+    df <- filedata()
+    genedict <- dict() # Create empty dict from container library
+    
+    for (i in 1:nrow(df)) { # Iterate through rows of the DF
+      l = df[[i,3]] # save list of split genes as "l"
+      for (e in l) { # Iterate through list of genes
+        if (!genedict$has(e)) { # check if dict already has gene in it and if not
+          new_entry <- list(c(df[[i,1]], df[[i,4]], df[[i,2]]))
+          genedict$set(e, new_entry, add=TRUE) # add GO with p-Value to dict as list
+        } else { # check if pValue of current row is smaller than previously saved and if yes
+          curr <- genedict$peek(e) # save current dict content for that gene
+          new_entry <- append(curr, list(c(df[[i,1]], df[[i,4]], df[[i,2]])))
+          genedict$set(e, new_entry, add=TRUE) # append current GO with p-Value to previous content and save
+        }
       }
     }
-  }
+    
+    results <- list() # create empty list 
+    for (k in genedict$keys()) { # iterate through genes in dict
+      dfk = transpose(data.frame(genedict$get(k))) # extract dict tuples into transposed df
+      dfk$V3 = as.numeric(dfk$V3) # convert results to num for ordering
+      names(dfk) = c("Gene Ontology", "Term", "FDR-Value")
+      results = append(results, list(dfk[order(dfk$"FDR-Value")[1:nrow(dfk)], ])) # [1:n] to save rows 1-n of ordered p-Values
+    }
+    names(results) = genedict$keys() # Name dfs in result list
+    results
+  })
   
-  results <- list() # create empty list 
-  for (k in genedict$keys()) { # iterate through genes in dict
-    dfk = transpose(data.frame(genedict$get(k))) # extract dict tuples into transposed df
-    dfk$V2 = as.numeric(dfk$V2) # convert results to num for ordering
-    results = append(results, list(dfk[order(dfk$V2)[1:3], ])) # save rows 1-3 of ordered p-Values
-  }
-  names(results) = genedict$keys() # Name dfs in result list
-  rm(list = c("e", "i", "k", "l", "curr", "dfk", "genedict")) # Remove intermediate variables
+  #The following set of functions populate the column selectors
+  output$gene <- renderUI({
+    df <-res()
+    if (is.null(df)) return(NULL)
+    
+    items=sort(names(df))
+    names(items)=items
+    selectInput("gene", "Gene", items)
+  })
   
+  output$inTab <- renderDataTable({
+    filedata()
+  })
   
-  output$tabl <- renderDataTable({
-    results[[input$Gene]]
+  output$resTab <- renderDataTable({
+    res()[[input$gene]]
   })
 }
 
 
-shinyApp(ui = ui, server = server)
 
+shinyApp(ui = ui, server = server)
